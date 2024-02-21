2d3696d323c601ff628a354235feba13b58d0912
Ankita <ankita.shah118@gmail.com>
2024-02-21 14:53:52 -0500

    Revert "Apex Screening"
    This reverts commit 958897bed5b194f10ab6e1509f2d9425ccd790c7.

--- apex-springboot-inventory-master-solved/src/main/java/com/apex/eqp/inventory/repositories/RecalledProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apex.eqp.inventory.repositories;
-
-import com.apex.eqp.inventory.entities.RecalledProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface RecalledProductRepository extends JpaRepository<RecalledProduct, Integer> {}
